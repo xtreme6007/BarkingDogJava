27d6a656dc243c34f16d3bd4f844b1520854ac1f
xtreme6007 <prestonnichols96@gmail.com>
2021-01-04 11:25:50 -0600

    working on activity

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class BarkingDog {
+public class BarkingDog {
+
+    public static boolean shouldWakeUp (boolean isBarking, int hourOfDay) {
+
+        if (hourOfDay < 0  || hourOfDay > 23) {
+            return false;
+        }
+        if (((hourOfDay < 8 && hourOfDay > 0  ) || (hourOfDay > 22 && hourOfDay < 23)) && isBarking == true) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+        }
+
+
+
+
+    }
 }
